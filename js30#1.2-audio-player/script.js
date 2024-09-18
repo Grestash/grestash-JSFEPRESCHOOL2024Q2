4e8eb08b513f86fbd150d5222534bf2e2a437d9f
Pavel Shliatski <pavelshlatcki1@gmail.com>
2024-09-18 10:35:05 +0300

    feat:added audio player functionality

--- js30#1.2-audio-player/script.js
+++ js30#1.2-audio-player/script.js
@@ -4,22 +4,89 @@ document.addEventListener('DOMContentLoaded', function() {
     const timeline = document.getElementById('timeline');
     const currentTimeText = document.getElementById('current-time');
     const durationText = document.getElementById('duration');
-    let isPlay = false;
+    const play_next_button = document.getElementById('play-next-button');
+    const play_prev_button = document.getElementById('play-prev-button');
+    const tracks = [
+        "assets/music/Travis_Scott-My_eyes.mp3",
+        "assets/music/Gunna-One_Of_Wun.mp3",
+    ];
+    let playNum = 0;
 
-    play_pause_button.addEventListener('click', function() {
-        
+    let isPlay = false;
+    audio.src = tracks[playNum];
+
+    play_pause_button.addEventListener('click', function playAudio() {
+
         if (!isPlay) {
             audio.play();
-            play_pause_button.textContent = 'Pause';
+            play_pause_button.innerHTML = '<img src="assets/icons/pause.png" alt="pause" id="pause-icon" class="play-pause-img">';
             isPlay = true;
-            
         } else {
             audio.pause();
-            play_pause_button.textContent = 'Play';
-            isPlay = false; 
-            
+            play_pause_button.innerHTML = '<img src="assets/icons/play.png" alt="play" id="play-icon"  class="play-pause-img">';
+            isPlay = false;
         }
     });
+
+
+    play_next_button.addEventListener("click", () => {
+        if (playNum < tracks.length - 1) {
+            playNum++;
+            audio.src = tracks[playNum];
+            play_pause_button.innerHTML = '<img src="assets/icons/pause.png" alt="pause" id="pause-icon" class="play-pause-img">';
+            isPlay = true;
+            timeline.value = 0;
+            audio.play();
+        } else {
+            playNum = 0;
+            audio.src = tracks[playNum];
+            play_pause_button.innerHTML = '<img src="assets/icons/pause.png" alt="pause" id="pause-icon" class="play-pause-img">';
+            isPlay= true;
+            timeline.value = 0;
+            audio.play();
+        }
+    });
+
+    play_prev_button.addEventListener("click", () => {
+        if (playNum > 0) {
+            playNum--;
+            audio.src = tracks[playNum];
+            play_pause_button.innerHTML = '<img src="assets/icons/pause.png" alt="pause" id="pause-icon" class="play-pause-img">';
+            isPlay = true;
+            audio.play();
+        } else {
+            playNum = tracks.length -1;
+            audio.src = tracks[playNum];
+            play_pause_button.innerHTML = '<img src="assets/icons/pause.png" alt="pause" id="pause-icon" class="play-pause-img">';
+            isPlay = true;
+            audio.play();
+        }
+    });
+
+    audio.addEventListener('ended', () => {
+        if (playNum < tracks.length - 1) {
+            playNum++;
+            audio.src = tracks[playNum];
+            audio.play();
+        } else {
+            playNum = 0;
+            audio.src = tracks[playNum];
+            audio.play();
+        }
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     audio.addEventListener('timeupdate', function() {
         const currentTime = audio.currentTime;
@@ -31,8 +98,7 @@ document.addEventListener('DOMContentLoaded', function() {
     })
 
     timeline.addEventListener('input', function() {
-        const seekTo = audio.duration * (timeline.value / 100);
-        audio.currentTime = seekTo;
+        audio.currentTime  = audio.duration * (timeline.value / 100);
     });
 
     function formatTime(time) {
