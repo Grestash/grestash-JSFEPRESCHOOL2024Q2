32cb4a463a6dfa3e211533250e96c6a3812467a4
Pavel Shliatski <pavelshlatcki1@gmail.com>
2025-02-03 19:25:08 +0300

    feat: made up burger-menu on the main page

---
+++ shelter/pages/main/script.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const burgerIcon = document.getElementById("burger-icon");
+  const nav = document.querySelector(".nav");
+  const navLinks = document.querySelectorAll(".nav-list a");
+  const overlay = document.getElementById("overlay");
+
+  function toggleMenu() {
+    nav.classList.toggle("nav-active");
+    nav.classList.remove("nav-hidden");
+    burgerIcon.classList.toggle("burger-icon-active");
+    overlay.classList.toggle("active");
+
+    if (nav.classList.contains("nav-active")) {
+      document.body.style.overflow = "hidden";
+      navLinks.forEach((link) => link.addEventListener("click", closeMenu));
+    } else {
+      document.body.style.overflow = "";
+      navLinks.forEach((link) => link.removeEventListener("click", closeMenu));
+    }
+  }
+
+  function closeMenu() {
+    nav.classList.remove("nav-active");
+    burgerIcon.classList.remove("burger-icon-active");
+    overlay.classList.remove("active");
+    document.body.style.overflow = "";
+  }
+
+  burgerIcon.addEventListener("click", toggleMenu);
+
+  overlay.addEventListener("click", toggleMenu);
+
+  window.addEventListener("resize", function () {
+    if (window.innerWidth > 768) {
+      nav.classList.add("nav-hidden");
+      closeMenu()
+    } else {
+      nav.classList.remove("nav-hidden");
+    }
+  });
+});
